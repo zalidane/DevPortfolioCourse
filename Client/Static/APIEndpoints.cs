82d10c1d50947f8268b2d35e69c989c0e41f8b9d
Antonio Delgado <adelgado@bellflight.com>
2021-10-11 16:17:11 -0500

    Consuming the API from the Client (2/2)

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Client.Static
+{
+    internal static class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "http://localhost:5003";
+#else
+        internal const string ServerBaseUrl = "http://appname.azurewebsites.net";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+    }
+}
